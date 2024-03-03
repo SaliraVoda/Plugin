696c066043e9a746dd896ceec3b440541557fc47
SaliraVoda <161962941+SaliraVoda@users.noreply.github.com>
2024-03-03 21:08:33 +0530

    Update pmpermit.py

--- TelethonHell/plugins/pmpermit.py
+++ TelethonHell/plugins/pmpermit.py
@@ -702,9 +702,9 @@ async def do_pm_permit_action(chat_ids, event, client):
             return
         except:
             pass
-    thee_message = "U cant pm lol {hell_mention}"     
+            hell_mention = utils.get_display_name(await event.client.get_me())
+    thee_message = f"U cant pm lol {hell_mention}"     
     PIC = "https://te.legra.ph/file/ea9e11f7c9db21c1b8d5e.mp4"
-    hell_mention = utils.get_display_name(await event.client.get_me())
     if chat_ids in client.PREV_REPLY_MESSAGE:
         await client.PREV_REPLY_MESSAGE[chat_ids].delete()
     tap = await event.client.send_file(
