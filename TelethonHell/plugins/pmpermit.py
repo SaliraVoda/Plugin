870e4e0a9008c91e3b1c788d641a6f065276a845
SaliraVoda <161962941+SaliraVoda@users.noreply.github.com>
2024-03-03 14:19:32 +0530

    Update pmpermit.py

--- TelethonHell/plugins/pmpermit.py
+++ TelethonHell/plugins/pmpermit.py
@@ -709,6 +709,7 @@ async def do_pm_permit_action(chat_ids, event, client):
         file=PIC,
         caption=thee_message,
         parse_mode="HTML",
+        reply_to=message_id,
     )
     client.PM_WARNS[chat_ids] += 1
     if chat_ids in client.PREV_REPLY_MESSAGE:
